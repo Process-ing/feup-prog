b05a7611c7bb2b99d33942150a72b384da24e750
processing <rbruno.p.o@gmail.com>
2023-04-29 16:08:31 +0100

    Add "replace" command to Script.

--- Script.hpp
+++ Script.hpp
@@ -26,6 +26,13 @@ namespace prog
     void save();
 
     /**
+     * @brief Implement "replace" command.
+     * @details The command "replace r1 g1 b1 r2 g2 b2" replaces all
+     *          (r1, g1, b1) pixels by (r2, g2, b2).
+     */
+    void replace();
+
+    /**
      * @brief Implements "h_mirror" command.
      * @details The command "h_mirror" mirrors the image horizontally.
      * @author Bruno Oliveira
