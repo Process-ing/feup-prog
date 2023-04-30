70716a62fb03af80c9ae183cc3ee3b442b7314b9
Memechanic <jpmmendes04@gmail.com>
2023-04-30 19:20:33 +0100

    implement Image

--- Image.cpp
+++ Image.cpp
@@ -2,31 +2,31 @@
 
 namespace prog
 {
-  Image::Image(int w, int h, const Color &fill)
-  {
-  }
+  Image::Image(int w, int h, const Color &fill):
+    width_(w), 
+    height_(h), 
+    data_(h, std::vector<Color>(w, fill))
+    {}
   Image::~Image()
   {
   }
   int Image::width() const
   {
-    return -1;
+    return width_;
   }
   int Image::height() const
   {
-    return -1;
+    return height_;
   }
 
-  // TODO: remove this DUMMY_color variable once you have appropriate fields for representing image pixels.
-  Color DUMMY_color;
 
   Color& Image::at(int x, int y)
   {
-    return DUMMY_color;
+    return data_[y][x];
   }
 
   const Color& Image::at(int x, int y) const
   {
-    return DUMMY_color;
+    return data_[y][x];
   }
 }
--- Image.hpp
+++ Image.hpp
@@ -1,19 +1,51 @@
 #ifndef __prog_Image_hpp__
 #define __prog_Image_hpp__
 #include "Color.hpp"
+#include <vector>
 
 namespace prog
 {
+  /**
+   * @brief 2D grid that represents an image using vectors that hold RGB color values
+   * 
+   */
   class Image
   {
   private:
-    // TODO: define private fields for image state
+    int width_;
+    int height_;
+    std::vector<std::vector<Color>> data_;
   public:
+    /**
+     * @brief Construct a new Image object
+     * 
+     * @param w Width
+     * @param h Height
+     * @param fill Fill color
+     */
     Image(int w, int h, const Color &fill = {255, 255, 255});
+    /**
+     * @brief Destroy the Image object, in this case, it is not necessary
+     * 
+     */
     ~Image();
     int width() const;
     int height() const;
+    /**
+     * @brief Mutable function that returns the wanted color value
+     * 
+     * @param x 
+     * @param y 
+     * @return Color& Mutable value
+     */
     Color &at(int x, int y);
+    /**
+     * @brief Constant function that returns the wanted color value
+     * 
+     * @param x 
+     * @param y 
+     * @return const Color& Constant value
+     */
     const Color &at(int x, int y) const;
   };
 }
